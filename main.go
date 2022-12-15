96eff2d74c662ccfcbe6ca3de979158aa710efd5
Rakibul Yeasin <ryeasin03@gmail.com>
2022-12-15 16:39:39 +0600

    Implemented some functions

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"binancebot/order"
 	"context"
 	"fmt"
 
 	"github.com/adshao/go-binance/v2"
-	"github.com/adshao/go-binance/v2/futures"
 )
 
 var (
@@ -28,52 +28,9 @@ func main() {
 	}
 
 	// Place order
-	marketEnterPosition(client)
-	marketExitPosition(client) // Exit
+	order.MarketEnterPosition(client)
+	order.MarketExitPosition(client) // Exit
 
 	// Place Limit Order
-	limitEnterPosition(client)
-}
-
-func marketEnterPosition(client *futures.Client) {
-	order, err := client.NewCreateOrderService().Symbol("BNBETH").
-		Side(futures.SideTypeBuy).Type(futures.OrderTypeMarket).
-		TimeInForce(futures.TimeInForceTypeGTC).Quantity("5").
-		Do(context.Background())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(order)
-
-	// Use Test() instead of Do() for testing.
-}
-
-func marketExitPosition(client *futures.Client) {
-	order, err := client.NewCreateOrderService().Symbol("BNBETH").
-		Side(futures.SideTypeBuy).Type(futures.OrderTypeMarket).
-		TimeInForce(futures.TimeInForceTypeGTC).Quantity("5").
-		ReduceOnly(true).
-		Do(context.Background())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(order)
-
-	// Use Test() instead of Do() for testing.
-}
-
-func limitEnterPosition(client *futures.Client) {
-	order, err := client.NewCreateOrderService().Symbol("BNBETH").
-		Side(futures.SideTypeBuy).Type(futures.OrderTypeMarket).
-		TimeInForce(futures.TimeInForceTypeGTC).Quantity("5").
-		Price("0.0030000").Do(context.Background())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(order)
-
-	// Use Test() instead of Do() for testing.
+	order.LimitEnterPosition(client)
 }
